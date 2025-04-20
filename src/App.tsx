77349419d1f4113b36cc55bee479c077c1027cbd
mirbek10 <m51869956@gmail.com>
2025-04-20 11:41:35 +0600

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { RouterProvider } from 'react-router-dom'
 import myRouter from './routes/router'
 
--- src/Page/UsersDetail/UserDetails.tsx
+++ src/Page/UsersDetail/UserDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../store/store";
